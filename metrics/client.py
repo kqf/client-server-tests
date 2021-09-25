69e74ac51f8d11b64cba9547a9e57fd24a245c1e
Sasha <q.dev.null@gmail.com>
2021-09-25 12:48:40 +0200

    Add put implementation

--- metrics/client.py
+++ metrics/client.py
@@ -1,6 +1,19 @@
+import time
+import socket
+
+
 class Client:
-    def put(self, metrics_name, metrics, timestamp):
-        pass
+    def __init__(self, ip="127.0.0.1", port=10001):
+        self._sock = socket.socket()
+        self._sock.connect((ip, port))
+
+    def put(self, metrics_name, metrics, timestamp=None):
+        timing = timestamp or int(time.time())
+        payload = f"put {metrics_name} {metrics} {timing}"
+        self._sock.sendall(payload.encode("utf8"))
 
     def get(self, metrics_name):
         pass
+
+    def close(self):
+        self._sock.close()
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,12 @@
 import pytest
+from contextlib import closing
 from metrics.client import Client
 
 
 @pytest.fixture
 def client():
-    return Client()
+    with closing(Client(ip="127.0.0.1", port=10001)) as client:
+        yield client
 
 
 def test_puts_metrics(client):
