e9c84a3dabf3888e8abf87ea917c3ebe3eba90f9
kqf <q.dev.null@gmail.com>
2021-09-26 12:46:09 +0200

    Add get method (#2)
    * Add the dummy client implementation
    
    * Add put implementation
    
    * Test reads metrics
    
    * Add a scatch for the get method
    
    * Add the test cases
    
    * Fix the get tests
    
    * Finalize the get method
    
    * Clean the linting errors

--- metrics/client.py
+++ metrics/client.py
@@ -1,6 +1,18 @@
 import time
 import socket
 
+from collections import defaultdict
+
+
+def _parse_metrics(message):
+    output = defaultdict(list)
+    for line in message.splitlines():
+        metrics_name, metrics, timestamp = line.split()
+        output[metrics_name].append(
+            (float(metrics), int(timestamp))
+        )
+    return output
+
 
 class Client:
     def __init__(self, ip="127.0.0.1", port=10001, timeout=None):
@@ -14,7 +26,9 @@ class Client:
         self._sock.sendall(payload.encode("utf8"))
 
     def get(self, metrics_name):
-        pass
+        self._sock.sendall(metrics_name.encode("utf8"))
+        response = self._sock.recv(1024).decode("utf8")
+        return _parse_metrics(response)
 
     def close(self):
         self._sock.close()
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,21 +1,24 @@
 import pytest
 import socket
+
+from unittest import mock
+
 from contextlib import closing
 from metrics.client import Client
-
 
 
 @pytest.fixture
 def addr(ip="127.0.0.1", port=10001):
     return {"ip": ip, "port": port}
 
- 
+
 @pytest.fixture
 def server(addr):
     with socket.socket() as sock:
         sock.bind(("", 10001))
-        sock.listen()   
+        sock.listen()
         yield sock
+
 
 @pytest.fixture
 def client(server, addr):
@@ -30,4 +33,35 @@ def test_puts_metrics(client):
     client.put("eardrum.cpu", 3, timestamp=1150864250)
     client.put("eardrum.cpu", 4, timestamp=1150864251)
     client.put("eardrum.memory", 4200000)
-    print(client.get("*"))
+
+
+@pytest.mark.parametrize("mname, server_response, expected", [
+    (
+        "palm.cpu",
+        "palm.cpu 0.5 111\npalm.cpu 0.6 112",
+        {
+            "palm.cpu": [
+                (0.5, 111),
+                (0.6, 112),
+            ]
+        }
+    ),
+    (
+        "*",
+        "palm.cpu 0.5 111\npalm.cpu 0.6 112\neardrum.cpu 0.8 113",
+        {
+            "palm.cpu": [
+                (0.5, 111),
+                (0.6, 112),
+            ],
+            "eardrum.cpu": [
+                (0.8, 113),
+            ]
+        }
+    ),
+])
+@mock.patch("socket.socket.recv")
+def test_gets_metrics(recv, mname, server_response, expected, client):
+    recv.return_value = server_response.encode("utf8")
+    outputs = client.get(mname)
+    assert outputs == expected
