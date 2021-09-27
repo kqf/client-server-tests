c258d6cbdcde51d919dac6ecb848ddf36794039c
Sasha <q.dev.null@gmail.com>
2021-09-27 21:42:36 +0200

    Add the simple protocol based implementation

--- metrics/server.py
+++ metrics/server.py
@@ -1,17 +1,25 @@
 import asyncio
 
 
-async def hello(reader, writer):
-    data = await reader.read(1024)
-    message = data.decode()
-    addr = writer.get_extra_info("peername")
-    print("received %r from %r" % (message, addr))
-    writer.close()
+def process_data(str):
+    pass
+
+
+class ClientServerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        resp = process_data(data.decode())
+        self.transport.write(resp.encode())
 
 
 def main():
     loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(hello, "127.0.0.1", 10001, loop=loop)
+    coro = asyncio.start_server(
+        ClientServerProtocol,
+        "127.0.0.1", 10001, loop=loop
+    )
     server = loop.run_until_complete(coro)
     try:
         loop.run_forever()
