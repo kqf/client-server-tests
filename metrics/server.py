5aba3cb4bdb8bcd07a9f5da103d6a910a8aa53a9
kqf <q.dev.null@gmail.com>
2021-09-27 21:48:19 +0200

    Use protocols (#6)
    * Add the simple protocol based implementation
    
    * Implement the basic preprocessing

--- metrics/server.py
+++ metrics/server.py
@@ -1,17 +1,35 @@
 import asyncio
+from functools import lru_cache
 
 
-async def hello(reader, writer):
-    data = await reader.read(1024)
-    message = data.decode()
-    addr = writer.get_extra_info("peername")
-    print("received %r from %r" % (message, addr))
-    writer.close()
+@lru_cache
+def process_data(message):
+    if message.startswith("get"):
+        _, metric_name = message.split()
+        return metric_name
+
+    if message.startswith("put"):
+        command, metric_name, metric, timestamp = message.split()
+        return {(metric_name, timestamp): metric}
+
+    raise NotImplemented("Other commands are not implemented")
+
+
+class ClientServerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        resp = process_data(data.decode())
+        self.transport.write(resp.encode())
 
 
 def main():
     loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(hello, "127.0.0.1", 10001, loop=loop)
+    coro = asyncio.start_server(
+        ClientServerProtocol,
+        "127.0.0.1", 10001, loop=loop
+    )
     server = loop.run_until_complete(coro)
     try:
         loop.run_forever()
