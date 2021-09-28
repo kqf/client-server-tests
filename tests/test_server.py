615b51e56341ec59c0c121028a86e85cba280530
Sasha <q.dev.null@gmail.com>
2021-09-28 06:41:47 +0200

    AdFix the warning

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,10 +3,7 @@ from metrics.server import ClientServerProtocol
 
 
 def test_metrics_server(event_loop):
-    coro = asyncio.start_server(
-        ClientServerProtocol,
-        "127.0.0.1", 10001, loop=event_loop
-    )
+    coro = asyncio.start_server(ClientServerProtocol, "127.0.0.1", 10001)
     server = event_loop.run_until_complete(coro)
 
     server.close()
