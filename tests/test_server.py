afc395706c727ea78dc4ca955af3ceb20425a13d
Sasha <q.dev.null@gmail.com>
2021-10-02 18:45:13 +0200

    Remove the lradditional put statement

--- tests/test_server.py
+++ tests/test_server.py
@@ -15,4 +15,3 @@ async def server(event_loop, addr):
 async def test_metrics_server(event_loop, server, client):
     client.put("palm.cpu", 0.5, timestamp=1150864247)
     await asyncio.sleep(0.01)
-    # client.put("palm.cpu", 0.5, timestamp=1150864247)
