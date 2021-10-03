bc67941b08b6ca03abefe0254513002dcbf3b891
Sasha <q.dev.null@gmail.com>
2021-10-03 12:19:48 +0200

    Add the http transport

---
+++ metrics/http.py
@@ -0,0 +1,20 @@
+
+
+class HttpTransport:
+    def __init__(self, transport):
+        self.transport = transport
+
+    def write(self, message):
+        self.transport.write(b"HTTP/1.1 200 OK\n\nHello World")
+
+def add_http_layer(cls):
+
+    class HttpWrapped(cls):
+        def connection_made(self, transport):
+            super().connection_made(transport)
+            self.transport = HttpTransport(transport)
+
+    def create_server():
+        return HttpWrapped()
+
+    return create_server
