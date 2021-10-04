f1ba203d83d7d439d4b1e7edbb6468a05c6d8ad2
kqf <q.dev.null@gmail.com>
2021-10-04 17:28:41 +0200

    Add http tests (#23)
    * Check adds the transport layer
    
    * Add the header tests

---
+++ tests/test_http.py
@@ -0,0 +1,31 @@
+from metrics.http import add_http_layer, HttpTransport
+
+
+class MockProtocol:
+    def connection_made(self, transport):
+        pass
+
+
+def test_adds_http_layer():
+    protocol = add_http_layer(MockProtocol)()
+    protocol.connection_made(None)
+    assert isinstance(protocol.transport, HttpTransport)
+
+
+class MockTransport:
+    def write(self, message):
+        self.write_params = message
+
+    def close(self):
+        self.called_close = True
+
+
+def test_adds_header():
+    transport = MockTransport()
+
+    decorated = HttpTransport(transport)
+    decorated.write("something")
+
+    message_sent = transport.write_params.decode("utf-8")
+    assert message_sent == "HTTP/1.1 200 OK\n\nHello World"
+    assert transport.called_close
