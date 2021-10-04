ea1e55dcb34eab58bf10f28afcd3e75e90f692d5
Sasha <q.dev.null@gmail.com>
2021-10-04 17:28:06 +0200

    Add the header tests

--- tests/test_http.py
+++ tests/test_http.py
@@ -10,3 +10,22 @@ def test_adds_http_layer():
     protocol = add_http_layer(MockProtocol)()
     protocol.connection_made(None)
     assert isinstance(protocol.transport, HttpTransport)
+
+
+class MockTransport:
+    def write(self, message):
+        self.write_params = message
+
+    def close(self):
+        self.called_close = True
+
+
+def test_adds_header():
+    transport = MockTransport()
+
+    decorated = HttpTransport(transport)
+    decorated.write("something")
+
+    message_sent = transport.write_params.decode("utf-8")
+    assert message_sent == "HTTP/1.1 200 OK\n\nHello World"
+    assert transport.called_close
