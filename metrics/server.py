3bbefd9f36f25882d01af5fe5ee23ee61ffa62a9
kqf <q.dev.null@gmail.com>
2021-10-05 10:40:52 +0200

    Add protocol tests (#25)
    * Separate the metrics protocol from the server
    
    * Bump the protocol coverage

--- metrics/server.py
+++ metrics/server.py
@@ -1,66 +1,13 @@
 import asyncio
-from time import sleep
-from collections import defaultdict
 from environs import Env
 
 
 from metrics.http import add_http_layer
+from metrics.protocol import MetricsProtocol
 
 
 env = Env()
 env.read_env()
-
-
-class MetricsProtocol:
-    _exposed_methods = {
-        "put",
-        "get",
-    }
-
-    def __init__(self, store=None, timeout=0):
-        self.store = store or defaultdict(dict)
-        self.timeout = timeout
-
-    def execute(self, message):
-        data = message.split()
-
-        try:
-            command, *_ = data
-        except ValueError:
-            return "error\nwrong command\n\n"
-
-        if command not in self._exposed_methods:
-            return "error\nwrong command\n\n"
-
-        method = getattr(self, command)
-        return method(message)
-
-    def put(self, message):
-        sleep(self.timeout)
-        try:
-            _, metric_name, metric, timestamp = message.split()
-            self.store[metric_name][int(timestamp)] = float(metric)
-        except ValueError:
-            return "error\nwrong command\n\n"
-        return "ok\n\n"
-
-    def get(self, message):
-        sleep(self.timeout)
-        try:
-            _, metric_name = message.split()
-        except ValueError:
-            return "error\nwrong command\n\n"
-
-        payload = "\n".join(self._ls(metric_name))
-        return f"ok\n{payload}\n\n"
-
-    def _ls(self, name):
-        for metric_name, entries in self.store.items():
-            if name != metric_name and name != "*":
-                continue
-
-            for timestamp, value in entries.items():
-                yield f"{metric_name} {value} {timestamp}"
 
 
 class ClientServerProtocol(asyncio.Protocol, MetricsProtocol):
